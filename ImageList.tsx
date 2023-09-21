8789e5730f32c3cd54f534f00a3489fe2398d546
moriagr <moriagro@gmail.com>
2023-09-21 14:47:58 +0300

    Adding animation fade in and out+ add double press

--- ImageList.tsx
+++ ImageList.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { View, Image, ImageBackground } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, TouchableWithoutFeedback, Animated } from 'react-native';
 import RNFS from 'react-native-fs';
-import { styles } from './my_style'
+import { styles } from './my_style';
 
 interface ImageListProps {
   directoryPath: string;
@@ -16,28 +16,39 @@ const ImageList: React.FC<ImageListProps> = ({ directoryPath }) => {
   const [imagePaths, setImagePaths] = useState<string[]>([]);
   const [shuffledImages, setShuffledImages] = useState<string[]>([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentTimer, setCurrentTimer] = useState(5000);
+  const [animationTimer, setAnimationTimer] = useState(500);
+  const doublePressThreshold = 300; // Adjust as needed (milliseconds)
+  const [lastPressTime, setLastPressTime] = useState(0);
+
+  // Use refs for animated values
+  const backgroundOpacity = useRef(new Animated.Value(0)).current;
+  const topImageOpacity = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     fetchImagesFromDirectory(directoryPath);
   }, [directoryPath]);
 
-
   useEffect(() => {
     const interval = setInterval(() => {
       // Increment the image index or reset to 0 if it reaches the end
       setCurrentImageIndex((prevIndex) =>
-        prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1
+        prevIndex === shuffledImages.length - 1 ? 0 : prevIndex + 1
       );
-    }, 5000); // 5000 milliseconds = 5 seconds
+
+      // Start the fade-in animation for both images
+      fadeInImages();
+    }, currentTimer + (animationTimer * 2)); // 5000 milliseconds = 5 seconds
 
     return () => clearInterval(interval); // Cleanup the interval when the component unmounts
   }, [shuffledImages]);
 
   useEffect(() => {
-    if (currentImageIndex == -1) {
-      shuffleArray(imagePaths)
+    if (currentImageIndex === 0) {
+      // If we've reached the end of the images, shuffle the array
+      shuffleArray(imagePaths);
     }
-  }, [currentImageIndex]);
+  }, [currentImageIndex, imagePaths]);
 
   const fetchImagesFromDirectory = async (path: string) => {
     try {
@@ -52,7 +63,7 @@ const ImageList: React.FC<ImageListProps> = ({ directoryPath }) => {
       const imagePaths = imageFiles.map((file: ImageFile) => file.path);
 
       setImagePaths(imagePaths);
-      shuffleArray(imagePaths)
+      shuffleArray(imagePaths);
     } catch (error) {
       console.error('Error reading directory:', error);
     }
@@ -79,11 +90,73 @@ const ImageList: React.FC<ImageListProps> = ({ directoryPath }) => {
     setShuffledImages(shuffledArray);
   };
 
+  const fadeInImages = () => {
+    // Reset opacity values to 0
+    backgroundOpacity.setValue(0);
+    topImageOpacity.setValue(0);
+
+    // Start the fade-in animation for both images
+    Animated.parallel([
+      Animated.timing(backgroundOpacity, {
+        toValue: 1,
+        duration: animationTimer,
+        useNativeDriver: false,
+      }),
+      Animated.timing(topImageOpacity, {
+        toValue: 1,
+        duration: animationTimer,
+        useNativeDriver: false,
+      }),
+    ]).start(() => {
+      // After 5 seconds, start the fade-out animation for both images
+      setTimeout(() => fadeOutImages(), currentTimer); // Delay of 4.5 seconds for a total of 5 seconds
+    });
+  };
+
+  const fadeOutImages = () => {
+    // Start the fade-out animation for both images
+    Animated.parallel([
+      Animated.timing(backgroundOpacity, {
+        toValue: 0,
+        duration: animationTimer,
+        useNativeDriver: false,
+      }),
+      Animated.timing(topImageOpacity, {
+        toValue: 0,
+        duration: animationTimer,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  const handleViewPress = () => {
+    const currentTime = Date.now();
+    const timeSinceLastPress = currentTime - lastPressTime;
+
+    if (timeSinceLastPress < doublePressThreshold) {
+      // Double press detected
+      console.log('Double press detected!');
+    }
+
+    setLastPressTime(currentTime);
+  };
+
   return (
-    <View>
-      <ImageBackground source={{ uri: 'file://' + shuffledImages[currentImageIndex] }} style={styles.backgroundImage} blurRadius={10} />
-      <Image source={{ uri: 'file://' + shuffledImages[currentImageIndex] }} style={styles.topImage} />
-    </View>
+    <TouchableWithoutFeedback onPress={handleViewPress}>
+
+      <View>
+        <Animated.Image
+          source={{ uri: 'file://' + shuffledImages[currentImageIndex] }}
+          style={[styles.backgroundImage, { opacity: backgroundOpacity }]}
+          blurRadius={10}
+        />
+        <Animated.Image
+          source={{ uri: 'file://' + shuffledImages[currentImageIndex] }}
+          style={[styles.topImage, { opacity: topImageOpacity }]}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+
   );
 };
 
